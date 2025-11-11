905623dc5fbeec706c92740f8081d44673a4c91b
Nanditha200 <mamathasathish451@gmail.com>
2025-11-11 22:10:50 +0530

    Create Add two numbers by user.py

---
+++ Add two numbers by user.py
@@ -0,0 +1,8 @@
+length = float(input("Enter length:"))
+width = float (input("Enter width: "))
+area = length*width
+perimeter = 2*(length + width)
+print("Area of rectangle:",area)
+print("perimeter of rectangle:", perimeter)
+print("Nanditha")
+print("AIML")
