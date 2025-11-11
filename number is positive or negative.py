d65719bb61d87ba660e26cbf8f77aeab6e20f747
Nanditha200 <mamathasathish451@gmail.com>
2025-11-11 15:01:38 +0530

    Create number is positive or negative.py

---
+++ number is positive or negative.py
@@ -0,0 +1,15 @@
+
+
+
+# program to check if a number is positive or negative 
+
+# take input from the user
+num=float(input("enter the number"))
+
+# check the number
+if num > 0:
+   print("the number is positive")
+elif num < 0:
+   print("the number is negative")
+else:
+   print("the number is zero")
